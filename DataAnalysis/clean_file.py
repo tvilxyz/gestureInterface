2c9c575e1c12cf2b822c70a6e69687ff7b7eded3
k4tho <katho@chapman.edu>
2023-07-11 15:39:15 -0700

    fixed clean_files.py and export_clean_files.py

--- DataAnalysis/clean_file.py
+++ DataAnalysis/clean_file.py
@@ -1,74 +1,178 @@
-# import necessary packages
+"""
+This script takes in a file containing gesture data and cleans it up by removing trials that have been striked out. 
+The user should only utilize the UpdateFile() as the other ones are used as helpers for that method.
+
+It requires pandas, os, and math to be installed in the environment.
+
+This tool accepts .csv files and assumes that the file will have an x_counter and gesture_counter_UI column for analytics.
+
+This script contains the following methods:
+    * CleanFile - Removes trials that have been striked and returns a cleaned dataframe.
+    * GetNumStrikes - Gets the total number of strikes, or max x_counter, found in the file.
+    * ReadFile - Converts a csv file into a dataframe.
+    * ExportToCSV - Converts the cleaned dataframe as a csv into a specified location.
+    * GetUpdatedFile - Reads, cleans, and exports a cleaned csv.
+"""
+
 import warnings
 warnings.filterwarnings('ignore')
 
-import csv
+import os
+import math
 import pandas as pd
-import numpy as np
-from plotnine import *
-import statsmodels.api as sm
-
-from sklearn.linear_model import LinearRegression # Linear Regression Model
-from sklearn.preprocessing import StandardScaler # Z-score variables
-from sklearn.preprocessing import MinMaxScaler # Min-Max Normalization
-
-from sklearn.model_selection import train_test_split # simple TT split cv
-
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
 
 
-def CleanFile(inputDF):
-    newDF = pd.DataFrame(data=None, columns = inputDF.columns, index=inputDF.index)
+
+def CleanFile(file):
+    """
+    Removes all of the gestures that have been striked out.
+    An increase in the x_counter column, meaning the x button was pressed, removes the most recent trial.
+
+    Iterates through testDF, a copy of the original dataframe, and adds that most recent trial for each gesture_counter_UI
+    into newDF before removing it from testDF. Repeats until all rows have been analyzed, and testDF is empty.
+
+    Parameters
+    -----
+    file : dataframe
+        dataframe to clean
+
+    Returns
+    -----
+    newDF : dataframe
+        a new dataframe that represents the cleaned version of the original csv file
+    """
+
+    newDF = pd.DataFrame(data=None, columns = file.columns, index=file.index)
     newDF = newDF[0:0]
-    tempDF = inputDF.copy()
-    uiCounter = -1                              #uiCounter keeps track of what the gestureCounterUI should be if things run smoothly
+    testDF = file.copy()
+    uiCounter = -1
     
-    while (tempDF.empty == False):
-        guiGroupNum = (tempDF.iloc[0]["gesture_counter_UI"]).astype(int)                
+    while (testDF.empty == False):
+        #Gets the gesture_counter_UI of the first row in testDF
+        guiGroupNum = (testDF.iloc[0]["gesture_counter_UI"]).astype(int)                
         uiCounter += 1                                                  
-        #Subject has started a new gesture successfully
-        #Adds the rows with the same gestureCounterUI and stops when the next row has a different number
-        #This could mean that a new gesture has started or the x was pressed.
-        if (guiGroupNum == uiCounter):                                  
-            for i in range(len(tempDF.index)):
-                if tempDF.iloc[0]["gesture_counter_UI"] == uiCounter:                 
-                    row = tempDF.iloc[0].copy()
+        if (guiGroupNum == uiCounter):              
+            # Iterates through testDF and adds row from testDf to newDF and removes that row from testDF
+            # until a new gesture has been recorded or a strike occurs                    
+            for i in range(len(testDF.index)):
+                if testDF.iloc[0]["gesture_counter_UI"] == uiCounter:                 
+                    row = testDF.iloc[0].copy()
                     newDF.loc[len(newDF.index)] = row
-                    tempDF = tempDF.iloc[1:]
+                    testDF = testDF.iloc[1:]
                 else:
+                    # Unique exception in data file when there is data in the last row is nAN
+                    # Ex) Sub10/Freeform_Sub10_Sess1/session_F_PanUp_subjID_10_05-10-23_02-37-19.csv
+                    if (math.isnan(testDF.iloc[0]["gesture_counter_UI"])):
+                        testDF = testDF.iloc[1:].dropna(how='all')
                     break
-        #x counter was pressed
-        #gets the gestureCounterUI of the last row in the newDF and deletes all of the rows with the same gestureCounterUI
         else:                                                           
+            # A strike has occured
+            # Removes the most recent trial from newDF, which is the rows at the tail with the same gesture_counter_UI number
+            if (newDF.empty == True):
+                continue
             uiCounter -= 3
             tailGroupNum = (newDF.iloc[-1]["gesture_counter_UI"])
             newDF.drop(newDF[newDF["gesture_counter_UI"] == tailGroupNum].index, inplace = True)
             newDF.reset_index(drop=True, inplace=True)
+            
     return newDF
     
-#Gets the number of strikes found in the file
-def GetNumStrikes(inputDF):
-    totalNumStrikes = inputDF["x_counter"].max()
+
+def GetNumStrikes(file):
+    """
+    Checks how many trials have been striked in the file.
+
+    Parameters
+    -----
+    file : dataframe
+        dataframe to clean
+
+    Returns
+    -----
+    totalNumStrikes : int
+        the amount of strikes(x_counter) a file contains
+    """
+
+    totalNumStrikes = file["x_counter"].max()
     return totalNumStrikes
 
-def ReadFile(fileName):
-    inputDF = pd.read_csv(fileName)
-    return inputDF
 
-def ExportToCSV(dataFrame, fileName):
-    path = 'C:\\Users\\Documents\\CPSC_Courses\\Personal\\VRelax\\gestureInterface\\CleanData'
+def ReadFile(filePath):
+    """
+    Reads a csv file based on the path and returns a new dataframe.
+
+    Parameters
+    -----
+    filePath : str
+        path to the csv file that needs to be cleaned
+    
+    Returns
+    -----
+    file : dataframe
+        dataframe for the input csv file
+    """
+
+    file = pd.read_csv(filePath)
+    return file
+
+def GetFileExportPath(fileName, folderPath):
+    """
+    Gets the path where the clean gesture data file will be exported to.
+
+    Parameters
+    ------
+    fileName : string
+        name of the original file
+    folderPath : string
+        path to the folder that the new csv will be exported to
+
+    Returns
+    -----
+    gestureFilePath : string
+        the path to the new gesture data file
+    """
+
     newFileName = "cleaned_" + fileName
-    dataFrame.to_csv(newFileName, header=True, index=False)
+    gestureFilePath = folderPath + "\\" + newFileName
+    return gestureFilePath
 
-#Detects strikes in inputFile
-#Exports an new clean csv if strikes are found and returns the inputFile if no strikes are found
-def GetUpdatedFile(fileName):
-    inputDF = ReadFile(fileName)
-    if GetNumStrikes(inputDF) > 0:
-        cleanedDF = CleanFile(inputDF)
-        ExportToCSV(cleanedDF, fileName)
-    else:
-        ExportToCSV(inputDF, fileName)
+def ExportToCSV(dataFrame, fileName, folderPath):
+    """ 
+    Exports a cleaned version of the csv file to a designated folder path.
 
-GetUpdatedFile("session_I_PanLeft_subjID_26_06-29-23_01-59-16.csv")
+    Parameters
+    -----
+    dataframe : dataframe
+        dataframe to clean
+    fileName : string
+        name of the csv file that was cleaned
+    folderPath : string
+        path to the folder that the new csv will be exported to
+    """
+
+    gestureFilePath = GetFileExportPath(fileName, folderPath)
+    if not os.path.exists(gestureFilePath):
+        dataFrame.to_csv(gestureFilePath, header=True, index=False)
+
+
+def GetUpdatedFile(filePath, folderPath):
+    """
+    Takes in a csv file and returns a clean one.
+
+    Parameters
+    -----
+    filePath : str
+        path to the csv file that needs to be cleaned
+    folderPath : string
+        path to the export location of the clean csv file
+    """
+
+    ogFile = ReadFile(filePath)
+    fileName = os.path.basename(filePath)
+    gestureFilePath = GetFileExportPath(fileName, folderPath)
+    if not os.path.exists(gestureFilePath):
+        if GetNumStrikes(ogFile) > 0:
+            cleanedFile = CleanFile(ogFile)
+            ExportToCSV(cleanedFile, fileName, folderPath)
+        else:
+            ExportToCSV(ogFile, fileName, folderPath)
--- DataAnalysis/export_clean_files.py
+++ DataAnalysis/export_clean_files.py
@@ -1,20 +1,55 @@
+"""
+This script utilizes a Data and CleanedData directory. It will iterate through a data directory, cleaning the data files within
+the create, while creating subfolders within the clean data directory and exporting those clean files to their respective paths.
+
+It assumes that there are two existing directories, a Data and CleanedData directory. Within the Data directory, there will 
+be two subFolders, Sub# and SessionFolder sequentially, which represents the subject's identification number and all of their
+sessions. The SessionFolder will contain files with all of the gesture data for that specific session. A sample path will 
+have the format of ..\VRelax\gestureInterface\Data\Sub#\SessionFolder\GestureDataFile where Sub# will be replaced with the 
+subject's number. SessionFolder and GestureDataFile can be any name but GestureDataFile must be a .csv
+
+The program requires os.path to be downloaded into the environment and utilizes the clean_file.py file. 
+"""
+
 # import necessary packages
 import warnings
 warnings.filterwarnings('ignore')
 
-import csv
-import pandas as pd
-import numpy as np
-from plotnine import *
-import statsmodels.api as sm
-
-from sklearn.linear_model import LinearRegression # Linear Regression Model
-from sklearn.preprocessing import StandardScaler # Z-score variables
-from sklearn.preprocessing import MinMaxScaler # Min-Max Normalization
-
-from sklearn.model_selection import train_test_split # simple TT split cv
-
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-
+import os.path 
 from clean_file import *
+
+
+# Relative paths to the Data and CleanedData folders
+dataFolderPath = "..\\VRelax\\gestureInterface\\Data"
+cleanDataFolderPath = "..\\VRelax\\gestureInterface\\CleanedData"
+
+# List of paths to all of the subjectNumber folders - Sub1/Sub2/Sub3/etc
+subjectNumberFolders = [f.path for f in os.scandir(dataFolderPath) if f.is_dir()] 
+
+for subjectNumber in range(len(subjectNumberFolders)):
+    # Checks for an existing subNum folder for each subject in the CleanData directory and creates one if there isn't one
+    subjectNumberInCleanDataFolderPath = cleanDataFolderPath + "\\Sub" + str(subjectNumber+1)
+    currSubNumPath = dataFolderPath + "\\Sub" + str(subjectNumber+1)
+    if not os.path.exists(subjectNumberInCleanDataFolderPath):
+        os.makedirs(subjectNumberInCleanDataFolderPath)
+
+    # List of paths for all of the session folders for the single subNum folder
+    currentSubjectNumberFolder = [f.path for f in os.scandir(currSubNumPath) if f.is_dir()]       
+
+    # Iterates through the session folders in that subNum folder
+    for sessionNumber in range(len(currentSubjectNumberFolder)):                      
+        
+        # Gets the current session folder path
+        currentSessionFolderPath = currentSubjectNumberFolder[sessionNumber]  
+
+        # Checks for an existing session folder in the CleanedData\SubNum path and creates one if there isn't
+        sessionNumberInCleanDataFolderPath = subjectNumberInCleanDataFolderPath + "\\" + os.path.basename(currentSessionFolderPath)
+        if not os.path.exists(sessionNumberInCleanDataFolderPath):
+            os.makedirs(sessionNumberInCleanDataFolderPath)
+
+        #iterates through all of the files in the session folder
+        for fileNum in range(len(os.listdir(currentSessionFolderPath))):
+            currentGestureFileName = (os.listdir(currentSessionFolderPath)[fileNum])
+            currentGestureFilePath = currentSessionFolderPath + "\\" + currentGestureFileName
+            GetUpdatedFile(currentGestureFilePath, sessionNumberInCleanDataFolderPath)
+    
